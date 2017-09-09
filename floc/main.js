f028ef10eaa6ca2ceba073ffc86a4d21acc77ac1
Nikolay Belikov <nikmail88@gmail.com>
2017-09-09 12:25:31 +0400

    исправлена ошибка в теге style
    исправлена ошибка изменения текста в теге style

--- floc/main.js
+++ floc/main.js
@@ -7,7 +7,14 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function addLongText() {
-  var code = "javascript: var a,w=document.createTreeWalker(document,NodeFilter.SHOW_TEXT);while(a=w.nextNode()){if(a.textContent.trim().length)a.textContent='The longest non-technical word in major dictionaries is floccinaucinihilipilification at 29 letters.'}";
+  var code = "" +
+    "javascript:" +
+    "var a,w=document.createTreeWalker(document,NodeFilter.SHOW_TEXT);" +
+    "while(a=w.nextNode()) {" +
+      "if(a.parentElement.tagName !== 'STYLE' && a.textContent.trim().length) {" +
+        "a.textContent='The longest non-technical word in major dictionaries is floccinaucinihilipilification at 29 letters.'" +
+      "}" +
+    "}";
 
   chrome.tabs.executeScript(null,
   { code: code });
@@ -16,7 +23,14 @@ function addLongText() {
 }
 
 function addLargeImages() {
-  var code = "javascript: var a,w=document.createTreeWalker(document,NodeFilter.SHOW_ELEMENT);while(a=w.nextNode()){if(a.tagName === 'IMG')a.src='http://lorempixel.com/g/1920/1400'}";
+  var code = "" +
+    "javascript:" +
+    "var a,w=document.createTreeWalker(document,NodeFilter.SHOW_ELEMENT);" +
+    "while(a=w.nextNode()) {" +
+      "if(a.tagName === 'IMG') {" +
+        "a.src='http://lorempixel.com/g/1920/1400'" +
+      "}" +
+    "}";
 
   chrome.tabs.executeScript(null,
   { code: code });
