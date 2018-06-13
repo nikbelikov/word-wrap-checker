ff85af7fa9d6773aaead411c9facbb53e3ee1811
Daniel Dubchenko <daniel217032001@gmail.com>
2018-06-13 22:02:55 +0300

    Добавлены новые фичи
    1)Возможность выбора длины текста и размеров изображения
    2)Текст теперь также заменяется в input с типоами text и submit
    3) Изображения теперь тянутся с placeholder.com

--- floc/main.js
+++ floc/main.js
@@ -1,39 +1,55 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var addLongTextButton = document.getElementById("add_long_text");
-  var addLargeImagesButton = document.getElementById("add_large_images");
+document.addEventListener("DOMContentLoaded", function () {
+    var addLongTextButton = document.getElementById("add_long_text");
+    var addLargeImagesButton = document.getElementById("add_large_images");
 
-  addLongTextButton.addEventListener("click", addLongText);
-  addLargeImagesButton.addEventListener("click", addLargeImages);
+    addLongTextButton.addEventListener("click", function () {
+        addLongText(document.getElementById("add_long_text_count").value);
+    });
+    addLargeImagesButton.addEventListener("click", function () {
+        addLargeImages(document.getElementById("add_long_images_size_w").value, document.getElementById("add_long_images_size_h").value)
+    });
 });
 
-function addLongText() {
-  var code = "" +
-    "javascript:" +
-    "var a,w=document.createTreeWalker(document,NodeFilter.SHOW_TEXT);" +
-    "while(a=w.nextNode()) {" +
-      "if(a.parentElement.tagName !== 'STYLE' && a.textContent.trim().length) {" +
-        "a.textContent='The longest non-technical word in major dictionaries is floccinaucinihilipilification at 29 letters.'" +
-      "}" +
-    "}";
+function addLongText(n) {
+    var text = 'The longest non-technical word in major dictionaries is floccinaucinihilipilification at 29 letters.';
+    n = parseInt(n);
+    if (n !== 0) {
+        for (var i = 0; i < n / 100; i++) {
+            text = text + 'The longest non-technical word in major dictionaries is floccinaucinihilipilification at 29 letters.';
+        }
+        text = text.substr(0, n);
+    }
+    var code = "" +
+        "javascript:" +
+        "var a,w=document.createTreeWalker(document,NodeFilter.SHOW_TEXT);" +
+        "while(a=w.nextNode()) {" +
+            "if(a.parentElement.tagName !== 'STYLE' && a.textContent.trim().length) {" +
+                "a.textContent='"+text+"'" +
+            "};" +
+        "}"+
+        "w = document.getElementsByTagName('input');" +
+        "for(var i=0;i<w.length;i++){" +
+            "if(w[i].type ==='text' || w[i].type ==='submit') {" +
+                "w[i].value = '"+text +"'"+
+            "}" +
+        "}";
 
-  chrome.tabs.executeScript(null,
-  { code: code });
+    chrome.tabs.executeScript(null,
+        {code: code});
 
-  window.close();
+    window.close();
 }
 
-function addLargeImages() {
-  var code = "" +
-    "javascript:" +
-    "var a,w=document.createTreeWalker(document,NodeFilter.SHOW_ELEMENT);" +
-    "while(a=w.nextNode()) {" +
-      "if(a.tagName === 'IMG') {" +
-        "a.src='http://lorempixel.com/g/1920/1400'" +
-      "}" +
-    "}";
+function addLargeImages(w, h) {
+    var code = "" +
+        "javascript:" +
+        "var img=document.getElementsByTagName('img');" +
+        "for(var i=0; i<img.length;i++) {" +
+            "img[i].src=\"https://via.placeholder.com/"+w+"x"+h+"\""+
+        "}";
 
-  chrome.tabs.executeScript(null,
-  { code: code });
+    chrome.tabs.executeScript(null,
+        {code: code});
 
-  window.close();
+    window.close();
 }
