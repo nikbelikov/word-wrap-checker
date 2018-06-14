c94a3c4052783adbd4d7cd6b852ec3ed27a588dd
Nikolay Belikov <nikmail88@gmail.com>
2018-06-14 12:32:21 +0300

    слайдер для количества символов текста

--- floc/main.js
+++ floc/main.js
@@ -1,53 +1,68 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var addLongTextButton = document.getElementById("add_long_text");
-    var addLargeImagesButton = document.getElementById("add_large_images");
+document.addEventListener('DOMContentLoaded', function () {
+  var addLongTextButton = document.getElementById('add_long_text');
+  var addLargeImagesButton = document.getElementById('add_large_images');
+  var addLongTextSlider = document.getElementById('add_long_text_count');
 
-    addLongTextButton.addEventListener("click", function () {
-        addLongText(document.getElementById("add_long_text_count").value);
-    });
-    addLargeImagesButton.addEventListener("click", function () {
-        addLargeImages(document.getElementById("add_long_images_size_w").value, document.getElementById("add_long_images_size_h").value)
-    });
+  addLongTextButton.addEventListener('click', function () {
+    var length = document.getElementById('add_long_text_count').value;
+
+    addLongText(length);
+  });
+
+  addLongTextSlider.addEventListener('input', function () {
+    var spanCounter = document.getElementById('add_long_text_length');
+    var value = addLongTextSlider.value;
+
+    spanCounter.innerText = value;
+  });
+
+  addLargeImagesButton.addEventListener('click', function () {
+    var width = document.getElementById('add_long_images_size_w').value;
+    var height = document.getElementById('add_long_images_size_h').value;
+
+    addLargeImages(width, height);
+  });
 });
 
 function addLongText(n) {
-    var text = '';
-    for (var i = 0; i < n / 100 + 1; i++) {
-        text = text + 'The longest non-technical word in major dictionaries is floccinaucinihilipilification at 29 letters.';
-    }
-    text = text.substr(0, n);
-    var code = "" +
-        "javascript:" +
-        "var a,w=document.createTreeWalker(document,NodeFilter.SHOW_TEXT);" +
-        "while(a=w.nextNode()) {" +
-            "if(a.parentElement.tagName !== 'STYLE' && a.textContent.trim().length) {" +
-                "a.textContent='"+text+"'" +
-            "};" +
-        "}"+
-        "w = document.getElementsByTagName('input');" +
-        "for(var i=0;i<w.length;i++){" +
-            "if(w[i].type ==='text' || w[i].type ==='submit') {" +
-                "w[i].value = '"+text +"'"+
-            "}" +
-        "}";
+  var text = '';
 
-    chrome.tabs.executeScript(null,
-        {code: code});
+  for (var i = 0; i < n / 100 + 1; i++) {
+    text = text + 'The longest non-technical word in major dictionaries is floccinaucinihilipilification at 29 letters.';
+  }
+  text = text.substr(0, n);
 
-    window.close();
+  var code = "" +
+    "javascript:" +
+    "var a,w=document.createTreeWalker(document,NodeFilter.SHOW_TEXT);" +
+    "while(a=w.nextNode()) {" +
+    "if(a.parentElement.tagName !== 'STYLE' && a.textContent.trim().length) {" +
+    "a.textContent='" + text + "'" +
+    "};" +
+    "}" +
+    "w = document.getElementsByTagName('input');" +
+    "for(var i=0;i<w.length;i++){" +
+    "if(w[i].type ==='text' || w[i].type ==='submit') {" +
+    "w[i].value = '" + text + "'" +
+    "}" +
+    "}";
+
+  chrome.tabs.executeScript(null, {code: code});
+
+  window.close();
 }
 
 function addLargeImages(w, h) {
-    var code = "" +
-        "javascript:" +
-        "var img=document.getElementsByTagName('img');" +
-        "for(var i=0; i<img.length;i++) {" +
-            "img[i].src=\"https://via.placeholder.com/"+w+"x"+h+"\";"+
-            "img[i].removeAttribute('srcset')" +
-        "}";
+  var code = "" +
+    "javascript:" +
+    "var img=document.getElementsByTagName('img');" +
+    "for(var i=0; i<img.length;i++) {" +
+    "img[i].src=\"https://via.placeholder.com/" + w + "x" + h + "\";" +
+    "img[i].removeAttribute('srcset')" +
+    "}";
 
-    chrome.tabs.executeScript(null,
-        {code: code});
+  chrome.tabs.executeScript(null,
+    {code: code});
 
-    window.close();
+  window.close();
 }
