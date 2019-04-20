73795640acc4d3e2996401ef64c475bcfef46372
Nikolay Belikov <nikmail88@gmail.com>
2019-04-20 13:19:42 +0300

    готовые разрешения для картинок

--- floc/main.js
+++ floc/main.js
@@ -1,39 +1,19 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const addTextButton = document.getElementById('add_text');
-  const addImagesButton = document.getElementById('add_images');
-  const textLengthSlider = document.getElementById('text_length_slider');
-  const imagesWidthSlider = document.getElementById('images_width_slider');
-  const imagesHeightSlider = document.getElementById('images_height_slider');
+const addLargeImages = (w, h) => {
+  const code = "" +
+    "javascript:" +
+    "var img=document.getElementsByTagName('img');" +
+    "for(var i=0; i<img.length;i++) {" +
+    "img[i].src=\"https://via.placeholder.com/" + w + "x" + h + "\";" +
+    "img[i].removeAttribute('srcset')" +
+    "}";
 
-  textLengthSlider.addEventListener('input', function () {
-    const spanCounter = document.getElementById('add_text_length');
-    spanCounter.innerText = textLengthSlider.value;
-  });
+  chrome.tabs.executeScript(null,
+    {code: code});
 
-  addTextButton.addEventListener('click', function () {
-    const length = document.getElementById('text_length_slider').value;
-    addLongText(length);
-  });
+  window.close();
+};
 
-  imagesWidthSlider.addEventListener('input', function () {
-    const spanCounter = document.getElementById('images_width');
-    spanCounter.innerText = imagesWidthSlider.value;
-  });
-
-  imagesHeightSlider.addEventListener('input', function () {
-    let spanCounter = document.getElementById('images_heigth');
-    spanCounter.innerText = imagesHeightSlider.value;
-  });
-
-  addImagesButton.addEventListener('click', function () {
-    const width = document.getElementById('images_width_slider').value;
-    const height = document.getElementById('images_height_slider').value;
-
-    addLargeImages(width, height);
-  });
-});
-
-function addLongText(n) {
+const addLongText = n => {
   let text = '';
 
   for (let i = 0; i < n / 100 + 1; i++) {
@@ -59,19 +39,76 @@ function addLongText(n) {
   chrome.tabs.executeScript(null, {code: code});
 
   window.close();
-}
+};
 
-function addLargeImages(w, h) {
-  const code = "" +
-    "javascript:" +
-    "var img=document.getElementsByTagName('img');" +
-    "for(var i=0; i<img.length;i++) {" +
-    "img[i].src=\"https://via.placeholder.com/" + w + "x" + h + "\";" +
-    "img[i].removeAttribute('srcset')" +
-    "}";
+const getElement = (id) => {
+  return document.getElementById(id);
+};
 
-  chrome.tabs.executeScript(null,
-    {code: code});
+const setImageSize = (width, height) => {
+  const widthText = getElement('images_width');
+  const heightText = getElement('images_heigth');
+  const widthSlider = getElement('images_width_slider');
+  const heightSlider = getElement('images_height_slider');
 
-  window.close();
-}
+  widthText.innerText = width;
+  heightText.innerText = height;
+
+  widthSlider.value = width;
+  heightSlider.value = height;
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+  const addTextButton = getElement('add_text');
+  const addImagesButton = getElement('add_images');
+  const textLengthSlider = getElement('text_length_slider');
+  const imagesWidthSlider = getElement('images_width_slider');
+  const imagesHeightSlider = getElement('images_height_slider');
+  const set1024 = getElement('set1024');
+  const set1600 = getElement('set1600');
+  const set1920 = getElement('set1920');
+  const set2000 = getElement('set2000');
+
+  textLengthSlider.addEventListener('input', () => {
+    const spanCounter = getElement('add_text_length');
+    spanCounter.innerText = textLengthSlider.value;
+  });
+
+  addTextButton.addEventListener('click', () => {
+    const length = getElement('text_length_slider').value;
+    addLongText(length);
+  });
+
+  imagesWidthSlider.addEventListener('input', () => {
+    const spanCounter = getElement('images_width');
+    spanCounter.innerText = imagesWidthSlider.value;
+  });
+
+  imagesHeightSlider.addEventListener('input', () => {
+    const spanCounter = getElement('images_heigth');
+    spanCounter.innerText = imagesHeightSlider.value;
+  });
+
+  addImagesButton.addEventListener('click', function () {
+    const width = getElement('images_width_slider').value;
+    const height = getElement('images_height_slider').value;
+
+    addLargeImages(width, height);
+  });
+
+  set1024.addEventListener('click', () => {
+    setImageSize(1024, 768);
+  });
+
+  set1600.addEventListener('click', () => {
+    setImageSize(1600, 1200);
+  });
+
+  set1920.addEventListener('click', () => {
+    setImageSize(1920, 1080);
+  });
+
+  set2000.addEventListener('click', () => {
+    setImageSize(2000, 2000);
+  });
+});
